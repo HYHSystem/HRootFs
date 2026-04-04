632165b6aea8a1b055676c14a300d32547a26f51
HEYAHONG <2229388563@qq.com>
2026-04-04 19:57:14 +0800

    添加HRootFs配置

--- include/Makefile/hrootfs.mk
+++ include/Makefile/hrootfs.mk
@@ -4,3 +4,17 @@ BUILDROOT_LOCAL_SOURCE ?= ${SRCDIR}/buildroot/
 BUILDROOT_GIT_REV := $(shell git --git-dir="${BUILDROOT_SOURCE}/.git" rev-parse HEAD )
 HROOTFS_SOURCE ?= ${TOPDIR}/script/
 HROOTFS_GIT_REV := $(shell git --git-dir="${HROOTFS_SOURCE}/.git" rev-parse HEAD )
+
+.PHONY: hrootfs_configure
+
+#将源代码复制到本地
+${STAMPDIR}/.hrootfs_configure_${HROOTFS_GIT_REV}:
+	@echo install hrootfs config!
+	@for defconfig in $(shell ls "${HROOTFS_SOURCE}/configs"); do ln -sf "${HROOTFS_SOURCE}/configs/$${defconfig}" "${BUILDROOT_LOCAL_SOURCE}/configs/$${defconfig}"; done
+	@touch ${STAMPDIR}/.hrootfs_configure_${HROOTFS_GIT_REV}
+
+
+hrootfs_configure: ${STAMPDIR}/.hrootfs_configure_${HROOTFS_GIT_REV}
+	@echo configure hrootfs!
+
+configure_step += hrootfs_configure
