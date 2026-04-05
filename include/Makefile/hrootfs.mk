aeb145ab991c3f7db14969d732dfa89611054749
HEYAHONG <2229388563@qq.com>
2026-04-06 00:57:21 +0800

    当script发生变更后，删除之前的时间戳

--- include/Makefile/hrootfs.mk
+++ include/Makefile/hrootfs.mk
@@ -10,6 +10,7 @@ HROOTFS_GIT_REV := $(shell git --git-dir="${HROOTFS_SOURCE}/.git" rev-parse HEAD
 #将源代码复制到本地
 ${STAMPDIR}/.hrootfs_configure_${HROOTFS_GIT_REV}:
 	@echo install hrootfs config!
+	@-rm -rf ${STAMPDIR}/.hrootfs_configure_*
 	@for defconfig in $(shell ls "${HROOTFS_SOURCE}/configs"); do ln -sf "${HROOTFS_SOURCE}/configs/$${defconfig}" "${BUILDROOT_LOCAL_SOURCE}/configs/$${defconfig}"; done
 	@ln -sf "${HROOTFS_SOURCE}/board/hrootfs" "${BUILDROOT_LOCAL_SOURCE}/board/hrootfs"
 	@echo  > "${BUILDROOT_LOCAL_SOURCE}/package/HRootFs.Config.in"
